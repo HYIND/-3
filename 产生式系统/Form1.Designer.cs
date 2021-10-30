67772a2c8e25074253816dc02c2764503bd075e5
HYIND <1255231699@qq.com>
2021-10-30 23:56:16 +0800

    添加项目文件。

---
+++ 产生式系统/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace 产生式系统
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.column1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.column2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(544, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "正向推理";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.column1,
+            this.column2});
+            this.listView1.Font = new System.Drawing.Font("宋体", 10F);
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(46, 77);
+            this.listView1.MaximumSize = new System.Drawing.Size(406, 360);
+            this.listView1.MinimumSize = new System.Drawing.Size(406, 360);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(406, 360);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // column1
+            // 
+            this.column1.Text = "前件";
+            this.column1.Width = 200;
+            // 
+            // column2
+            // 
+            this.column2.Text = "后件";
+            this.column2.Width = 200;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(75, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "规则库";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(544, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 42);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "反向推理";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(544, 298);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 42);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "修改规则库";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "产生式系统";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.ColumnHeader column1;
+        public System.Windows.Forms.ColumnHeader column2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ 产生式系统/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace 产生式系统
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("rule_group.txt", Encoding.UTF8);
+            int linecount = 0;
+            while (!sr.EndOfStream)
+            {
+                string s = sr.ReadLine();
+                int count = 0;
+                int head = 0, length = s.Length;
+                char a = s[count];
+                string t = null;
+                while (++count != length)
+                {
+                    if (a != '-' && a != '>' && a != '，' && a != ',')
+                    {
+                        a = s[count];
+                    }
+                    else if (a == '，' || a == ',')
+                    {
+
+                        t = s.Substring(head, count - head - 1);
+                        head = count;
+                        a = s[count];
+                    }
+                    else if (a == '-')
+                    {
+                        string aute_temp = s.Substring(0, count - 1);
+                        listView1.Items.Add(aute_temp);
+                        t = s.Substring(head, count - head - 1);
+                        count++;
+                        head = count;
+                        a = s[count];
+                    }
+                }
+                t = s.Substring(head, count - head);
+                listView1.Items[linecount].SubItems.Add(t);
+                linecount++;
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
